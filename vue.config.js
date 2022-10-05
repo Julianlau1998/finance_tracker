97627a56f83b80133481c38407c6d94b41de8ec2
Julian Lau <julian.lau@aol.com>
2022-10-05 15:01:08 +0200

    color

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   pwa: {
     name: 'Electricity Cost Calculator',
-    themeColor: '#1a1c1e',
+    themeColor: '#2a343c',
     msTileColor: '#1a1c1e',
     navigationColor: "#1a1c1e",
     navigationColorDark: "#1a1c1e",
     navigationDividerColor: "#1a1c1e",
     navigationDividerColorDark: "#1a1c1e",
-    backgroundColor: '#1a1c1e',
+    backgroundColor: '#2a343c',
     mobileWebAppCapable: 'yes',
     appleMobileWebAppStatusBarStyle: 'default',
     start_url: '.',
